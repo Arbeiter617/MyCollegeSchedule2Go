497ae08595a09793ac488274cfaf826ce4181e43
Ian Fischer <ianfischer476@gmail.com>
2024-05-25 23:22:25 -0500

    added saving and port forwarding

--- bin/baseCode/script.js
+++ bin/baseCode/script.js
@@ -1,3 +1,6 @@
+// common arrays
+var nameArr = [];
+
 // variables
 var tempEmail;
 var tempMessage;
@@ -11,10 +14,14 @@ var cricketEnding = ["@mms.cricketwireless.net"];
 
 // value grabber
 function grabCellProvider(provider) {
-    for(var i = 0; i < window[provider].length; i++) {
-        tempEmail = document.querySelector(".phone_number_input").value.toString() + window[provider][i];
-        console.log("You are using " + provider);
-        console.log(tempEmail);
+    if(document.querySelector(".phone_number_input").value != "") {
+        for(var i = 0; i < window[provider].length; i++) {
+            tempEmail = document.querySelector(".phone_number_input").value.toString() + window[provider][i];
+            console.log("You are using " + provider);
+            console.log(tempEmail);
+        }
+    } else {
+        console.log("Please add an input");
     }
 }
 
@@ -22,7 +29,49 @@ function grabValue(btn) {
     var localName;
     if(btn == "submit") {
         localName = document.querySelector(".name_input").value;
-        console.log(localName);
+        if(localName == "#clear") {
+            cache();
+            console.log("cache cleared");
+        } else {
+            nameArr.push(localName);
+            saveData();
+        }
+    }
+}
+
+function grabCollegeData(college) {
+    if(college == "ULL") {
+        // for ULL
+        window.open('./images/campusMaps/ullCampusMap.png', '_blank').focus();
     }
 }
 //
+
+// data functions
+function initializeLocalData() {
+    nameArr = localStorage.saveLocatName.split(",");
+    console.log(nameArr);
+}
+
+function saveData() {
+    if (localStorage && 'saveLocatName' in localStorage) {
+        console.log("Current Data:" + localStorage.saveLocatName);
+    }
+
+    function storeData() {
+        localStorage && (localStorage.saveLocatName = [nameArr]);
+        console.log("Data saved: " + localStorage.saveLocatName);
+    }
+    storeData();
+}
+
+// to clear cache
+const cache = () => {
+    localStorage.clear();
+    localStorage.clear();
+ };
+//
+//
+
+// startup functions
+initializeLocalData();
